939e2e983883b038a08e968901dd70bc382b2ace
tkcjesse <tkcjesse@gmail.com>
2016-01-26 21:51:10 -0600

    Update arma3life.sql

--- arma3life.sql
+++ arma3life.sql
@@ -98,7 +98,6 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
   `plate` int(20) NOT NULL,
   `color` int(20) NOT NULL,
   `inventory` varchar(500) NOT NULL,
-  `gear` text NOT NULL,
   `fuel` double NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `side` (`side`),
